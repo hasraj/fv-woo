5687930c6af4575e932ce6886f9a68bcf98e02c4
hasan_raj <abuazam45@gmail.com>
2019-05-05 11:30:11 +0600

    Initial files
    This is initial files

---
+++ functions.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Storefront automatically loads the core CSS even if using a child theme as it is more efficient
+ * than @importing it in the child theme style.css file.
+ *
+ * Uncomment the line below if you'd like to disable the Storefront Core CSS.
+ *
+ * If you don't plan to dequeue the Storefront Core CSS you can remove the subsequent line and as well
+ * as the sf_child_theme_dequeue_style() function declaration.
+ */
+//add_action( 'wp_enqueue_scripts', 'sf_child_theme_dequeue_style', 999 );
+
+/**
+ * Dequeue the Storefront Parent theme core CSS
+ */
+function sf_child_theme_dequeue_style() {
+    wp_dequeue_style( 'storefront-style' );
+    wp_dequeue_style( 'storefront-woocommerce-style' );
+}
+
+/**
+ * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
+ */
+
+
+
+//add_action( 'wp_head', 'remove_my_action' );
+function remove_my_action(){
+	remove_action( 'homepage', 'storefront_homepage_content', 10 );
+	remove_action( 'homepage', 'storefront_product_categories', 20 );
+	remove_action( 'homepage', 'storefront_recent_products', 30 );
+	remove_action( 'homepage', 'storefront_featured_products', 40 );
+	remove_action( 'homepage', 'storefront_popular_products', 50 );
+	remove_action( 'homepage', 'storefront_on_sale_products', 60 );
+	remove_action( 'homepage', 'storefront_best_selling_products', 70 );
+	
+	remove_action( 'storefront_header', 'storefront_header_cart', 60 );
+}
